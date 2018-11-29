3808c2f76464823d9174993df589bcae18edaf61
patorseing <patorseing@gmail.com>
2018-11-29 18:51:54 +0700

    fix #3

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,9 @@ import { Route, Switch, Router } from "react-router-dom";
 import createBrowserHistory from 'history/createBrowserHistory'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Alert } from 'reactstrap';
-import SignIn from './views/SignIn'
 
 const history = createBrowserHistory()
-/*
+
 const LoadingComponant = ({ isLoading, error }) => {
     if (isLoading) {
         return <Alert color="info" style={{ margin: '25% 5%' }}><FontAwesomeIcon icon="spinner" spin /> Loading... </Alert>;
@@ -25,7 +24,7 @@ const SignIn = Loadable({
     loader: () => import('./views/SignIn'),
     loading: LoadingComponant
 })
-*/
+
 const Routes = props => {
     return (
         <App>

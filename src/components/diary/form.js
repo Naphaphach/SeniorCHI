04d7e19a95dd06b8202ca62398fe61981aa38fc8
patorseing <patorseing@gmail.com>
2018-12-29 07:13:56 +0700

    check point: upload multi files and preview

--- src/components/diary/form.js
+++ src/components/diary/form.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Paper, Grid, FormGroup, FormControlLabel, Switch, TextField } from '@material-ui/core/'
+import { GridListTileBar, GridListTile, GridList, IconButton, Button, MenuItem, Paper, Grid, FormGroup, FormControlLabel, Switch, TextField } from '@material-ui/core/'
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import SaveIcon from '@material-ui/icons/Save';
+import { state } from '../../models/state.json'
+import { Col } from 'reactstrap'
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
 const styles = theme => ({
     colorSwitchBase: {
@@ -16,28 +21,89 @@ const styles = theme => ({
     colorBar: {},
     colorChecked: {},
     root: {
-        flexGrow: 1,
-        marginLeft: '64px',
-        alignItems: 'flex-start !important'
-    },
-    rootmod: {
-        flexGrow: 1,
+        width: '100%'
     },
     paper: {
+        [theme.breakpoints.up('sm')]: {
+            marginTop: '8.5%',
+        },
+        [theme.breakpoints.down('sm')]: {
+            margin: '27.5% 0',
+        },
         padding: theme.spacing.unit * 2,
         textAlign: 'center',
         color: 'navy',
     },
     add: {
         fontSize: '2em'
-    }
+    },
+    margin: {
+        margin: theme.spacing.unit,
+    },
+    cssLabel: {
+        '&$cssFocused': {
+            color: '#FF9933',
+        },
+    },
+    cssFocused: {},
+    cssUnderline: {
+        '&:after': {
+            borderBottomColor: '#FF9933',
+        },
+    },
+    cssOutlinedInput: {
+        '&$cssFocused $notchedOutline': {
+            borderColor: '#FF9933',
+        },
+    },
+    notchedOutline: {},
+    button: {
+        color: '#FF9933',
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing.unit * 2,
+    },
+    menu: {
+        minWidth: 200,
+    },
+    input: {
+        display: 'none',
+    },
+    im: {
+        height: '100%',
+        width: '100%',
+        cursor: 'pointer'
+    },
+    titleBar: {
+        background:
+            'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+            'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    icon: {
+        color: 'white',
+    },
 });
 
 class EditForm extends Component {
-    state = {
-        public: false,
-        text: 'private'
-    };
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            public: false,
+            text: 'private',
+            state: '',
+            labelWidth: 0,
+            imgfile: [],
+            files: [],
+            hide: true
+        };
+        this.handleButtonPress = this.handleButtonPress.bind(this)
+        this.handleButtonRelease = this.handleButtonRelease.bind(this)
+    }
 
     handleChange = name => event => {
         this.setState({ [name]: event.target.checked });
@@ -47,6 +113,45 @@ class EditForm extends Component {
             this.setState({ text: 'private' })
         }
     };
+
+    handleChangeField = name => event => {
+        this.setState({ [name]: event.target.value });
+    };
+
+    handleButtonPress() {
+        this.buttonPressTimer = setTimeout(() => this.setState({ hide: false }), 1500)
+        clearInterval(this.timerID);
+    }
+
+    handleButtonRelease() {
+        clearTimeout(this.buttonPressTimer);
+        this.timerID = setInterval(() => this.setState({ hide: true }), 1500)
+    }
+
+    //display multi
+    handleChangeImg(event) {
+        const files = Array.from(event.target.files);
+        files.map(img =>
+            this.setState(prevState => ({
+                imgfile: [...prevState.imgfile, URL.createObjectURL(img)],
+                files: [...prevState.files, img]
+            }))
+        )
+    }
+
+    componentDidUpdate(){
+        console.log(this.state);
+        
+    }
+
+    imageClick = (im) => {
+        const i = this.state.imgfile.indexOf(im)
+        this.state.files.splice(i, 1)
+        this.state.imgfile.splice(i, 1)
+        this.setState({ files: this.state.files })
+        this.setState({ imgfile: this.state.imgfile })
+    }
+
     render() {
         const { classes } = this.props
         return (
@@ -59,7 +164,7 @@ class EditForm extends Component {
                                     <Switch
                                         checked={this.state.public}
                                         onChange={this.handleChange('public')}
-                                        value="checkedA"
+                                        value="public"
                                         classes={{
                                             switchBase: classes.colorSwitchBase,
                                             checked: classes.colorChecked,
@@ -69,6 +174,29 @@ class EditForm extends Component {
                                 }
                                 label={this.state.text}
                             />
+                            <div className={classes.root}>
+                                <GridList cellHeight={160} cols={3}>
+                                    {this.state.imgfile.map(im => (
+                                        <GridListTile key={im} cols={1}>
+                                            <img src={im} alt={im} className={classes.im}
+                                                onTouchStart={this.handleButtonPress}
+                                                onTouchEnd={this.handleButtonRelease}
+                                                onMouseDown={this.handleButtonPress}
+                                                onMouseUp={this.handleButtonRelease} />
+                                            {!this.state.hide ? <GridListTileBar
+                                                titlePosition="top"
+                                                actionIcon={
+                                                    <IconButton className={classes.icon} onClick={() => this.imageClick(im)}>
+                                                        <DeleteOutlineIcon />
+                                                    </IconButton>
+                                                }
+                                                actionPosition="left"
+                                                className={classes.titleBar}
+                                            /> : null}
+                                        </GridListTile>
+                                    ))}
+                                </GridList>
+                            </div>
                             <TextField
                                 id="outlined-full-width"
                                 label="title"
@@ -77,23 +205,119 @@ class EditForm extends Component {
                                 fullWidth
                                 margin="normal"
                                 variant="outlined"
+                                className={classes.margin}
                                 InputLabelProps={{
-                                    shrink: true,
+                                    classes: {
+                                        root: classes.cssLabel,
+                                        focused: classes.cssFocused,
+                                    },
+                                }}
+                                InputProps={{
+                                    classes: {
+                                        root: classes.cssOutlinedInput,
+                                        focused: classes.cssFocused,
+                                        notchedOutline: classes.notchedOutline,
+                                    },
                                 }}
                             />
                             <TextField
-                                id="outlined-full-width"
-                                label="title"
+                                id="outlined-select-currency"
+                                select
+                                label="State"
                                 style={{ margin: 8 }}
-                                placeholder="title"
+                                SelectProps={{
+                                    MenuProps: {
+                                        className: classes.menu,
+                                    },
+                                }}
+                                InputLabelProps={{
+                                    classes: {
+                                        root: classes.cssLabel,
+                                        focused: classes.cssFocused,
+                                    },
+                                }}
+                                InputProps={{
+                                    classes: {
+                                        root: classes.cssOutlinedInput,
+                                        focused: classes.cssFocused,
+                                        notchedOutline: classes.notchedOutline,
+                                    },
+                                }}
+                                className={classes.textField}
+                                value={this.state.state}
+                                onChange={this.handleChangeField('state')}
+                                margin="normal"
+                                variant="outlined"
+                            >
+                                {state.map((option, i) => (
+                                    <MenuItem key={i} value={option}>
+                                        {option}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                            <TextField
+                                id="outlined-full-width"
+                                label="note"
+                                rows="4"
+                                style={{ margin: 8 }}
+                                placeholder="take a note..."
                                 fullWidth
                                 margin="normal"
                                 variant="outlined"
+                                className={classes.margin}
                                 InputLabelProps={{
-                                    shrink: true,
+                                    classes: {
+                                        root: classes.cssLabel,
+                                        focused: classes.cssFocused,
+                                    },
+                                }}
+                                InputProps={{
+                                    classes: {
+                                        root: classes.cssOutlinedInput,
+                                        focused: classes.cssFocused,
+                                        notchedOutline: classes.notchedOutline,
+                                    },
                                 }}
                                 multiline
                             />
+                            <TextField
+                                id="outlined-full-width"
+                                label="tag"
+                                rows="4"
+                                style={{ margin: 8 }}
+                                placeholder="eg local india food, india market"
+                                fullWidth
+                                margin="normal"
+                                variant="outlined"
+                                className={classes.margin}
+                                InputLabelProps={{
+                                    classes: {
+                                        root: classes.cssLabel,
+                                        focused: classes.cssFocused,
+                                    },
+                                }}
+                                InputProps={{
+                                    classes: {
+                                        root: classes.cssOutlinedInput,
+                                        focused: classes.cssFocused,
+                                        notchedOutline: classes.notchedOutline,
+                                    },
+                                }}
+                                multiline
+                            />
+                            <Col xs='7'></Col>
+                            <Col xs='5'>
+                                <input accept="image/*" className={classes.input} onChange={this.handleChangeImg.bind(this)} id="icon-button-file" type="file" multiple />
+                                <label htmlFor="icon-button-file">
+                                    <IconButton className={classes.button} component="span">
+                                        <PhotoCamera />
+                                    </IconButton>
+                                </label>
+                                <Button variant="contained" size="small" className={classes.button}>
+                                    <SaveIcon />
+                                    Save
+                                </Button>
+                            </Col>
                         </FormGroup>
                     </Paper>
                 </Grid>
--- src/store/actions/imgAction.js
+++ src/store/actions/imgAction.js
@@ -1,6 +1,5 @@
 export const changeImgProfile = (img) => {
     return (dispatch, getState) => {
-        console.log(img);
         dispatch({ type: 'CHANGE_IMAGE_PROFILE', img })
     }
 }

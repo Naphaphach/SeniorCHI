1c60a881b22ae2a1f6a814c8b5dc1822aa2eec35
naphaphach <naphaphach.pra@gmail.com>
2019-01-06 21:47:45 +0700

    start rotate function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-
+const diaryImgModule = require('./diary/image')
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -9,3 +9,5 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.Chi = functions.https.onRequest((request, response) => {
     response.send("Hello from Chi!");
 });
+
+exports.uploadedIMG = functions.storage.object().onFinalize(diaryImgModule.handler);

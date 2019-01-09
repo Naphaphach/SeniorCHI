c442ba6a9311cbc665af68047402bb863dc8b772
naphaphach <naphaphach.pra@gmail.com>
2019-01-09 13:57:44 +0700

    restart functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-const diaryImgModule = require('./diary/image')
+
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -10,4 +10,3 @@ exports.Chi = functions.https.onRequest((request, response) => {
     response.send("Hello from Chi!");
 });
 
-exports.uploadedIMG = functions.storage.object().onFinalize(diaryImgModule.handler);

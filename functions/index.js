1be044a6cfca2de63b6ea27eead83300596f15d0
patorseing <patorseing@gmail.com>
2019-01-19 22:23:15 +0700

    start resize image

--- functions/index.js
+++ functions/index.js
@@ -17,5 +17,8 @@ exports.UpdateTokenDiaryActivity = functions.firestore.document('user/{userID}/d
 const notificationTokenModule = require('./accounts/notificationTokenActivity')
 exports.notifyToken = functions.firestore.document('user/{userID}').onWrite(notificationTokenModule.handler)
 
-const imageDiaryModule = require('./diary/imageUpload')
-exports.rotateUsingExif = functions.storage.object().onFinalize(imageDiaryModule.handler)
+const imageDiaryRotateModule = require('./diary/imageRotate')
+exports.rotateUsingExif = functions.storage.object().onFinalize(imageDiaryRotateModule.handler)
+
+const imageDiaryResizeModule = require('./diary/imageResize')
+exports.resizeImage = functions.storage.object().onFinalize(imageDiaryResizeModule.handler)

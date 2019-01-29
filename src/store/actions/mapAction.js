6d8fc9ce0f2a01750fe2e4b9ce013e860b7bbd90
patorseing <patorseing@gmail.com>
2019-01-29 20:36:52 +0700

    fix rules

--- src/store/actions/mapAction.js
+++ src/store/actions/mapAction.js
@@ -1,5 +1,22 @@
+function compare(a, b) {
+    if (a.data.date > b.data.date)
+        return -1;
+    if (a.data.date < b.data.date)
+        return 1;
+    return 0;
+}
+
 export const changeState = (S) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        const firestore = getFirestore();
+
+        firestore.collection('diary').get().then(snapshot => {
+            const result = []
+            // snapshot.docs.map(doc => S === doc.data().state && doc.data().writer === state.firebase.auth.uid && doc.data().public ? result.push({ id: doc.id, data: doc.data() }) : null)
+            snapshot.docs.map(doc => doc.data().public ? console.log(doc.data()) : null)
+            result.sort(compare)
+            // console.log(snapshot);
+        })
         dispatch({ type: 'CHANGE_STATE', S })
     }
 }
